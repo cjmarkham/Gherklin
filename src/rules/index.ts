c5ef197aeed64e86edbd581b50f478b529466799
Carl Markham <carl.markham@elasticpath.com>
2024-08-22 11:12:45 +0100

    add name-length rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,9 +1,11 @@
 import allowedTags from './allowed-tags'
 import indentation from './indentation'
 import maxScenariosPerFile from './max-scenarios-per-file'
+import nameLength from './name-length'
 
 export default {
   allowedTags,
   indentation,
   maxScenariosPerFile,
+  nameLength,
 }
