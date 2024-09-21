b264083b661d0f0b99f2df0aa06e8a728ba41050
Carl Markham <carl.markham@elasticpath.com>
2024-09-22 00:36:18 +0100

    another attempt

--- gherklin.config.ts
+++ gherklin.config.ts
@@ -26,6 +26,6 @@ export default {
     'no-background-only': 'warn',
     'no-empty-file': 'warn',
     'no-trailing-spaces': 'warn',
-    'no-unnamed-scenarios': 'warn',
+    'no-unnamed-scenarios': 'error',
   },
 }
--- src/reporters/html_reporter.ts
+++ src/reporters/html_reporter.ts
@@ -1,9 +1,9 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
 import Handlebars from 'handlebars'
-import { v4 } from 'uuid'
 
 import Reporter from './reporter'
+import { Severity } from '../types'
 
 export default class HTMLReporter extends Reporter {
   public override write = (): void => {
@@ -13,54 +13,84 @@ export default class HTMLReporter extends Reporter {
     const values = {
       title: this.config?.title || 'Gherklin Report',
       files: [],
+      totalErrors: 0,
+      totalWarns: 0,
+      totalLines: 0,
+      rules: {},
     }
 
-    const keywords = ['Feature', 'Scenario', 'Given', 'When', 'Then', 'And', 'But', 'Examples']
+    const keywords = [
+      'Feature',
+      'Scenario',
+      'Given',
+      'When',
+      'Then',
+      'And',
+      'But',
+      'Examples',
+      'Background',
+      'Scenario Outline',
+    ]
 
     for (const [key] of this.errors.entries()) {
       const content = readFileSync(key, { encoding: 'utf-8' })
       const lines = content.split('\n')
-      const value = {
-        anchor: key.replace(this.config.configDirectory, '').replaceAll('/', '-'),
-        name: key.replace(this.config.configDirectory, ''),
+
+      const errors = this.errors.get(key)
+      const hasErrors = errors.some((e) => e.severity === Severity.error)
+      const fileInfo = {
+        path: key,
+        hasErrors,
         lines: [],
+        issueCount: errors.length,
       }
 
-      lines.forEach((line, index) => {
-        const errors = this.errors.get(key)
-        const lineError = errors.filter((e) => e.location.line === index + 1)?.[0]
-        const keywordMatch = line.match(new RegExp(`^${keywords.join('|')}`))
-        let keyword
-        if (keywordMatch) {
-          keyword = keywordMatch[0]
-          line = line.replace(keyword, '')
-          if (keyword === 'Feature') {
-          }
-        }
+      values.totalErrors += errors.map((e) => e.severity === Severity.error).length
+      values.totalWarns += errors.map((e) => e.severity === Severity.warn).length
+      values.totalLines += lines.length
 
-        let keywordFormatted
-        if (keyword) {
-          if (['Feature', 'Scenario', 'Examples'].includes(keyword)) {
-            keywordFormatted = `${keyword}`
-          } else {
-            keywordFormatted = `${keyword} `
-          }
-        }
+      lines.forEach((line: string, index: number) => {
+        const lineIssue = errors.find((e) => e.location.line === index + 1)
+        let keyword = undefined
+        let lineContent = line
+        let padding = lineContent.length
 
-        const lineValue = {
-          id: v4(),
-          content: line.trim(),
+        keywords.forEach((kw: string) => {
+          const keywordMatches = line.trim().match(new RegExp(`^${kw}`))
+          if (keywordMatches) {
+            keyword = keywordMatches[0].trim()
+            lineContent = lineContent.replace(keyword, '')
+            padding = (lineContent.length - lineContent.trim().length) / 2
+            lineContent = lineContent.trim()
+            if (!['Feature', 'Scenario', 'Scenario Outline', 'Background'].includes(kw)) {
+              lineContent = ' ' + lineContent
+            }
+          }
+        })
+
+        const lineInfo = {
           number: index + 1,
-          keyword: keyword && keywordFormatted,
-          column: line.length - line.trim().length,
-          padding: (line.length - line.trim().length) * 6,
-          hasError: lineError !== undefined,
-          error: lineError,
-        } as any
+          hasError: lineIssue,
+          errorSeverity: lineIssue && lineIssue.severity.toString().toLowerCase(),
+          keyword,
+          content: lineContent,
+          padding,
+          ruleName: lineIssue && lineIssue.rule,
+          ruleDescrition: 'Something here',
+        }
 
-        value.lines.push(lineValue)
+        if (lineIssue) {
+          if (lineIssue.rule in values.rules) {
+            values.rules[lineIssue.rule] += 1
+          } else {
+            values.rules[lineIssue.rule] = 1
+          }
+        }
+
+        fileInfo.lines.push(lineInfo)
       })
-      values.files.push(value)
+
+      values.files.push(fileInfo)
     }
 
     const html = template(values)
