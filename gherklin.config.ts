1cfdb99bf401384a68dd652e5acdadc02e784be0
Carl Markham <carl.markham@elasticpath.com>
2024-09-22 11:06:30 +0100

    improve html reporter and add stdout and json reporters

--- gherklin.config.ts
+++ gherklin.config.ts
@@ -4,6 +4,27 @@ export default {
   },
   featureDirectory: './tests/acceptance/features',
   rules: {
-    'no-unnamed-scenarios': 'off',
+    indentation: [
+      'warn',
+      {
+        feature: 1,
+        background: 3,
+        scenario: 3,
+        step: 5,
+        examples: 5,
+        given: 5,
+        when: 5,
+        then: 5,
+        and: 5,
+        but: 5,
+        exampleTableHeader: 7,
+        exampleTableBody: 7,
+      },
+    ],
+    'new-line-at-eof': 'warn',
+    'no-background-only': 'warn',
+    'no-empty-file': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-unnamed-scenarios': 'error',
   },
 }
