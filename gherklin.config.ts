3fcb977a3eb197f5cf7ec9df7d34511c56c5f8bd
Carl Markham <carl.markham@elasticpath.com>
2024-09-23 18:56:25 +0100

    convert all rules to classes

--- gherklin.config.ts
+++ gherklin.config.ts
@@ -1,6 +1,30 @@
 export default {
+  reporter: {
+    type: 'stdout',
+  },
   featureDirectory: './tests/acceptance/features',
   rules: {
-    'no-empty-file': 'on',
+    indentation: [
+      'warn',
+      {
+        feature: 1,
+        background: 3,
+        scenario: 3,
+        step: 5,
+        examples: 5,
+        given: 5,
+        when: 5,
+        then: 5,
+        and: 5,
+        but: 5,
+        exampleTableHeader: 7,
+        exampleTableBody: 7,
+      },
+    ],
+    'new-line-at-eof': 'warn',
+    'no-background-only': 'warn',
+    'no-empty-file': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-unnamed-scenarios': 'warn',
   },
 }
