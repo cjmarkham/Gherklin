6593c11462ed70721c78e894a894806c800586ef
Carl Markham <carl.markham@elasticpath.com>
2024-09-23 18:56:52 +0100

    convert all rules to classes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Runner } from './runner'
-export { default as Rule } from './rule_loader'
+export { Rule } from './rule'
 export type { LintError } from './error'
 export * as Schemas from './schemas'
 export { Severity, Switch } from './types'
