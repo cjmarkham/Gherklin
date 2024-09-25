ac8822b018dee6e7420a2c0292ee664f0b6a96e9
Carl Markham <carl.markham@elasticpath.com>
2024-09-25 21:17:17 +0100

    split document class up

--- src/document.ts
+++ src/document.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
-import { Feature, IdGenerator } from '@cucumber/messages'
+import { Feature, IdGenerator, GherkinDocument } from '@cucumber/messages'
 import Gherkin from '@cucumber/gherkin'
 
 import { LintError, Location } from './types'
@@ -13,6 +13,8 @@ export default class Document {
   public feature: Feature = new Feature()
 
   public path: string
+
+  public gherkinDocument: GherkinDocument
 
   // Sometimes, we need access to the raw lines since Gherkin has processing
   // to trim line content in the AST
@@ -40,44 +42,27 @@ export default class Document {
       throw new Error(`Could not open the feature file at "${this.filename}". Does it exist?`)
     })
 
+    this.parseGherkin(String(content))
+    if (!this.gherkinDocument) {
+      return
+    }
+
+    const lines = String(content).split(/\r\n|\r|\n/)
+    lines.forEach((line) => {
+      this.lines.push(new Line(line))
+    })
+
+    this.getDisabledRules()
+  }
+
+  private parseGherkin = (content: string): void => {
     const builder = new Gherkin.AstBuilder(IdGenerator.uuid())
     const matcher = new Gherkin.GherkinClassicTokenMatcher()
     const parser = new Gherkin.Parser(builder, matcher)
 
-    const gherkinDocument = parser.parse(content.toString())
-    if (!gherkinDocument) {
-      return
-    }
-
-    if (gherkinDocument.feature) {
-      this.feature = gherkinDocument.feature
-      const lines = String(content).split(/\r\n|\r|\n/)
-      lines.forEach((line) => {
-        this.lines.push(new Line(line))
-      })
-
-      gherkinDocument.comments.forEach((comment) => {
-        const text = comment.text.trim()
-
-        if (comment.location.line === 1) {
-          if (text === '# gherklin-disable') {
-            this.disabled = true
-            return
-          }
-
-          const disableRuleMatch = text.match(/^# gherklin-disable ([a-zA-Z0-9-,\s]+)$/)
-          if (disableRuleMatch) {
-            const rules = (disableRuleMatch[1] || '').split(',')
-            rules.forEach((rule) => {
-              this.rulesDisabled.set(rule.trim(), true)
-            })
-          }
-        }
-
-        if (text === '# gherklin-disable-next-line') {
-          this.linesDisabled.set(comment.location.line + 1, true)
-        }
-      })
+    this.gherkinDocument = parser.parse(content.toString())
+    if (this.gherkinDocument.feature) {
+      this.feature = this.gherkinDocument.feature
     }
   }
 
@@ -97,6 +82,31 @@ export default class Document {
     } as LintError)
   }
 
+  private getDisabledRules = (): void => {
+    this.gherkinDocument.comments.forEach((comment) => {
+      const text = comment.text.trim()
+
+      if (comment.location.line === 1) {
+        if (text === '# gherklin-disable') {
+          this.disabled = true
+          return
+        }
+
+        const disableRuleMatch = text.match(/^# gherklin-disable ([a-zA-Z0-9-,\s]+)$/)
+        if (disableRuleMatch) {
+          const rules = (disableRuleMatch[1] || '').split(',')
+          rules.forEach((rule) => {
+            this.rulesDisabled.set(rule.trim(), true)
+          })
+        }
+      }
+
+      if (text === '# gherklin-disable-next-line') {
+        this.linesDisabled.set(comment.location.line + 1, true)
+      }
+    })
+  }
+
   /**
    * Regenerates the file from the lines array, overwriting the existing file
    */
@@ -110,9 +120,11 @@ export default class Document {
       }
       lines.push(padding.join('') + l.keyword + l.text)
     })
-    writeFileSync(this.path, lines.join('\n'))
+
+    const content = lines.join('\n')
+    writeFileSync(this.path, content)
 
     // Need to regenerate the Gherkin AST since some rules rely on that
-    await this.load()
+    this.parseGherkin(content)
   }
 }
