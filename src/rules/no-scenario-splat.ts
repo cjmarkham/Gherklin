45c56c1fe5b190fb0a357d6aacd46c4fcc235f7c
Carl Markham <carl.markham@elasticpath.com>
2024-09-25 22:05:24 +0100

    add no-scenario-splat rule

--- src/rules/no-scenario-splat.ts
+++ src/rules/no-scenario-splat.ts
@@ -3,7 +3,6 @@ import Schema from '../schema'
 import Rule from '../rule'
 import { RawSchema, AcceptedSchema } from '../types'
 import Document from '../document'
-import Line from '../line'
 
 export default class NoScenarioSplat implements Rule {
   public readonly name: string = 'no-scenario-splat'
