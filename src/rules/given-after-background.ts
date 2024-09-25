d550a2173390606284372708cada4a419584edf5
Carl Markham <doddsey65@gmail.com>
2024-09-25 23:25:36 +0100

    Add given-after-background rule (#65)
    * add given-after-background rule
    
    * add given-after-background rule

---
+++ src/rules/given-after-background.ts
@@ -0,0 +1,36 @@
+import { switchOrSeveritySchema } from '../schemas'
+import Schema from '../schema'
+import Rule from '../rule'
+import { RawSchema, AcceptedSchema } from '../types'
+import Document from '../document'
+
+export default class GivenAfterBackground implements Rule {
+  public readonly name: string = 'given-after-background'
+
+  public readonly acceptedSchema: AcceptedSchema = switchOrSeveritySchema
+
+  public readonly schema: Schema
+
+  public constructor(rawSchema: RawSchema) {
+    this.schema = new Schema(rawSchema)
+  }
+
+  public async run(document: Document): Promise<void> {
+    const backgrounds = document.feature.children.filter((child) => child.background !== undefined)
+    if (!backgrounds.length) {
+      return
+    }
+
+    document.feature.children.forEach((child): void => {
+      if (!child.scenario) {
+        return
+      }
+
+      child.scenario.steps.forEach((step): void => {
+        if (step.keyword.trim() === 'Given') {
+          document.addError(this.name, 'Found "Given" in scenario when background exists.', step.location)
+        }
+      })
+    })
+  }
+}
